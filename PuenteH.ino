a04c7dde87718ee0dc2278c2100686a7c201e65b
Elías Zavala Sánchez <elias.zavala@usach.cl>
2020-09-09 11:21:35 -0300

    Codigo del puente H comentado

--- PuenteH.ino
+++ PuenteH.ino
@@ -1,24 +1,31 @@
 void setup()
 {
+  /*se designan los pines a utilizar, todos son de salida*/
   pinMode(8, OUTPUT);
   pinMode(9, OUTPUT);
   pinMode(13,OUTPUT);
 }
 
 void CambiaLed(){
+  /*se llama para verificar al pin 13*/
   digitalWrite(13,!digitalRead(13));
+  /*el signo de esclamación significa que se desea hacer la función contraria */
 }
 
 void loop()
 {
   digitalWrite(9, HIGH);
   digitalWrite(8, LOW);
-  delay(1000); // Espera 1 segundo (1000 milisegundos)
+  delay(1000);// Espera 1 segundo (1000 milisegundos)
+  /*Se establece una orientacion inicial y un delay que permite el movimiento del motor*/
   digitalWrite(9, HIGH);
   digitalWrite(8, HIGH);
-  delay(500); // Espera 1 segundo (1000 milisegundos)
+  delay(1000);// Espera 1 segundo (1000 milisegundos)
+  /*Cambio de señal genera el cambio de dirección y el delay es el tiempo de rotación*/
   digitalWrite(8, HIGH);
   digitalWrite(9, LOW);
-  delay(1000); // Espera 1 segundo (1000 milisegundos)
+  delay(1000);// Espera 1 segundo (1000 milisegundos)
+  /*Cambio de señal genera el cambio de dirección y el delay es el tiempo de rotación*/
   CambiaLed();
+  /*Se llama a la función que enciende el led del pin 13,comprobar el buen funcionamientp del circuito*/
 }
